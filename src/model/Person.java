f86edd4f6c05b9ca1e92fc607f98acde99e82db0
mkalmo <mart.kalmo@gmail.com>
2013-10-14 12:19:31 +0300

    starting state

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,35 +1,12 @@
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
-@Entity
 public class Person {
 
     @Id
     @SequenceGenerator(name = "my_seq", sequenceName = "SEQ1", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_seq")
     private Long id;
-
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
 }
